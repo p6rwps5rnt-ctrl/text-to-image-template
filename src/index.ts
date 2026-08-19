180de5091011750ddc21e9188013813edbb19f7e
p6rwps5rnt-ctrl <p6rwps5rnt@privaterelay.appleid.com>
2026-08-19 03:25:18 -0400

    Update index.ts
    Changed image prompt

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export default {
 	async fetch(_request: Request, env: Env): Promise<Response> {
 		const inputs = {
-			prompt: "cyberpunk cat",
+			prompt: "a brown and tan shih tzu pug mix in the midst of a visually stunning time traveling adventure",
 		} satisfies AiTextToImageInput;
 
 		const response =
